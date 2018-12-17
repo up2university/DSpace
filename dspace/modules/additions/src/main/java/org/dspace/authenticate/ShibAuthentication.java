f808a7b1bd19ab333df3a08d89693f4aa91249d6
Nick Mastoris <nmastoris@cti.gr>
2018-12-17 16:07:11 +0200

    send xAPI statement for Shibboleth Authentication

--- dspace/modules/additions/src/main/java/org/dspace/authenticate/ShibAuthentication.java
+++ dspace/modules/additions/src/main/java/org/dspace/authenticate/ShibAuthentication.java
@@ -233,7 +233,7 @@ public class ShibAuthentication implements AuthenticationMethod
 			log.info(eperson.getEmail()+" has been authenticated via shibboleth..");
 			if(eperson.getEmail()!=null) {
             	LrsUtils.sendLoginStatement(eperson.getID().toString(),username);
-            	log.info("send login statement");
+            	log.info("send login statement"+eperson.getID()+"--"+username);
         	}
 			return AuthenticationMethod.SUCCESS;
 
