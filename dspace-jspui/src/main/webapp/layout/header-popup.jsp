0a1867518b6895fc388c803a4d7a98289987f0e1
Nick Mastoris <nmastoris@cti.gr>
2019-01-14 10:49:50 +0200

    add lrsEnabled flag

--- dspace-jspui/src/main/webapp/layout/header-popup.jsp
+++ dspace-jspui/src/main/webapp/layout/header-popup.jsp
@@ -33,6 +33,7 @@
     String feedRef = (String)request.getAttribute("dspace.layout.feedref");
     List parts = (List)request.getAttribute("dspace.layout.linkparts");
     String extraHeadData = (String)request.getAttribute("dspace.layout.head");
+    
 %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
--- dspace/modules/additions/src/main/java/org/dspace/lrs/LrsUtils.java
+++ dspace/modules/additions/src/main/java/org/dspace/lrs/LrsUtils.java
@@ -27,7 +27,8 @@ import com.rusticisoftware.tincan.lrsresponses.StatementLRSResponse;
 
 public final class LrsUtils {
 	private static RemoteLRS lrs = new RemoteLRS(TCAPIVersion.V100);
-
+	private static Boolean lrsEnabled=false;
+	
 	/** SLF4J logging category */
 	private final static Logger log = (Logger) LoggerFactory.getLogger(LrsUtils.class);
 
@@ -38,6 +39,7 @@ public final class LrsUtils {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
+		lrsEnabled=Boolean.valueOf(ConfigurationManager.getProperty("lrs.enabled"));
 		lrs.setUsername(ConfigurationManager.getProperty("lrs.username"));
 		lrs.setPassword(ConfigurationManager.getProperty("lrs.password"));
 		log.info("USERNAME" + lrs.getUsername());
@@ -45,214 +47,221 @@ public final class LrsUtils {
 
 	public static void sendLoginStatement(String id, String username) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(id);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("https://brindlewaye.com/xAPITerms/verbs/loggedin");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if(lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(id);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("https://brindlewaye.com/xAPITerms/verbs/loggedin");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "loggedin");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/community-site/dspace","http://id.tincanapi.com/activitytype/community-site","Up2U DSpace Community","Up2U repository for users where they can upload multimedia content.");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "loggedin");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/community-site/dspace","http://id.tincanapi.com/activitytype/community-site","Up2U DSpace Community","Up2U repository for users where they can upload multimedia content.");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
 	}
 
 	public static void sendCreateNewSubmissionStatement(String userId, String username,String collectionName,String collectionId, String itemId) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://activitystrea.ms/schema/1.0/create");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://activitystrea.ms/schema/1.0/create");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "created");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
+					"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			statement.setContext(createContext(collectionId,collectionName));
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "created");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
-				"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		statement.setContext(createContext(collectionId,collectionName));
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendDeleteSubmissionStatement(String userId, String username,String collectionName,String collectionId, String itemId) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://activitystrea.ms/schema/1.0/delete");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://activitystrea.ms/schema/1.0/delete");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "deleted");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
+					"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			statement.setContext(createContext(collectionId,collectionName));
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "deleted");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
-				"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		statement.setContext(createContext(collectionId,collectionName));
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendUpdateSubmissionStatement(String userId, String username,String collectionName,String collectionId, String itemId) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://activitystrea.ms/schema/1.0/update");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://activitystrea.ms/schema/1.0/update");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "updated");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
+					"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			statement.setContext(createContext(collectionId,collectionName));
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "updated");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
-				"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		statement.setContext(createContext(collectionId,collectionName));
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendSubmitSubmissionStatement(String userId, String username,String collectionName,String collectionId, String itemId) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://activitystrea.ms/schema/1.0/submit");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://activitystrea.ms/schema/1.0/submit");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "submitted");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
+					"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			statement.setContext(createContext(collectionId,collectionName));
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "submitted");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
-				"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		statement.setContext(createContext(collectionId,collectionName));
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendAccessedItemStatement(String userId, String username,String collectionName,String collectionId, String itemId) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://id.tincanapi.com/verb/viewed");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://id.tincanapi.com/verb/viewed");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "viewed");
+	
+			Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
+					"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			statement.setContext(createContext(collectionId,collectionName));
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "viewed");
-
-		Activity activity = createActivity("http://id.tincanapi.com/activitytype/resource/"+itemId,
-				"http://id.tincanapi.com/activitytype/resource",itemId,"Resource can be a video,image or text");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		statement.setContext(createContext(collectionId,collectionName));
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendSearchQueryStatement(String userId, String username,String pageUrl) {
 		// *LRS - Statement*//
-
-		Agent agent = new Agent();
-		agent.setMbox("mailto:" + username);
-		agent.setName(userId);
-
-		Verb verb = null;
-		try {
-			verb = new Verb("http://activitystrea.ms/schema/1.0/search");
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if (lrsEnabled) {
+			Agent agent = new Agent();
+			agent.setMbox("mailto:" + username);
+			agent.setName(userId);
+	
+			Verb verb = null;
+			try {
+				verb = new Verb("http://activitystrea.ms/schema/1.0/search");
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			verb.setDisplay(new LanguageMap());
+			verb.getDisplay().put("en-US", "searched");
+	
+			Activity activity = createActivity(pageUrl,
+					"http://activitystrea.ms/schema/1.0/page","DSpace search page","");
+	
+			Statement statement = new Statement();
+			statement.setActor(agent);
+			statement.setVerb(verb);
+			statement.setObject(activity);
+			
+			log.info(statement.getActor().getName());
+			StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+			log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 		}
-		verb.setDisplay(new LanguageMap());
-		verb.getDisplay().put("en-US", "searched");
-
-		Activity activity = createActivity(pageUrl,
-				"http://activitystrea.ms/schema/1.0/page","DSpace search page","");
-
-		Statement statement = new Statement();
-		statement.setActor(agent);
-		statement.setVerb(verb);
-		statement.setObject(activity);
-		
-		log.info(statement.getActor().getName());
-		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
-		log.info(lrsRes.getErrMsg()+lrsRes.getSuccess());
 	}
 	
 	public static void sendLogoutStatement(String id, String username) {
 		// *LRS - Statement*//
-
+		if (lrsEnabled) {
 		Agent agent = new Agent();
 		agent.setMbox("mailto:" + username);
 		agent.setName(id);
@@ -275,10 +284,12 @@ public final class LrsUtils {
 		statement.setObject(activity);
 		log.info(statement.getActor().getName());
 		StatementLRSResponse lrsRes = lrs.saveStatement(statement);
+		}
 	}
 
 	public static Activity createActivity(String activityUri,
 			String definitionUri,String definitionName,String definitionDescription) {
+		
 		Activity activity = new Activity();
 		try {
 			activity.setId(new URI(activityUri));
@@ -300,6 +311,7 @@ public final class LrsUtils {
 				definitionDescription);
 		
 		return activity;
+		
 	}
 	public static Context createContext(String collectionId,String collectionTitle) {
 	 Context context = new Context();
